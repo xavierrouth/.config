542037b015c61ed638923a170489f1f81bcc1dfc
Xavier Routh <xavierrouth@gmail.com>
2025-09-03 08:32:11 -0500

    initial commit?

--- hypr/scripts/cycle.sh
+++ hypr/scripts/cycle.sh
@@ -4,22 +4,23 @@
 current_workspace=$(hyprctl activeworkspace | grep -oP '(?<=ID )\d+')
 
 shift_amount=$1
+move=$2
 next_workspace=$((current_workspace + shift_amount))
 
-echo $current_workspace
+# echo $current_workspace
 
 # Define workspace range
 if [[ current_workspace -ge 6 ]] && [[ current_workspace -le 10 ]]; then
-echo $next_workspace
-    echo "here"
+# echo $next_workspace
+    # echo "here"
     if [[ next_workspace -eq 11 ]]; then
         next_workspace=6
     elif [[ next_workspace -eq 5 ]]; then
         next_workspace=10
     fi
 elif [[ current_workspace -ge 1 ]] && [[ current_workspace -le 5 ]]; then
-echo "here2"
-echo $next_workspace
+# echo "here2"
+# echo $next_workspace
     if [[ next_workspace -eq 6 ]]; then
         next_workspace=0
     elif [[ next_workspace -eq 0 ]]; then
@@ -29,4 +30,12 @@ fi
 
 # Switch to the next workspace
 echo $next_workspace
-hyprctl dispatch workspace $next_workspace
+
+# If it is set to move, then move workspace
+
+if [[ "$move" = "yes" ]]; then
+    # $(hyprctl activewindow | grep)
+    hyprctl dispatch movetoworkspace $next_workspace
+else
+    hyprctl dispatch workspace $next_workspace
+fi
